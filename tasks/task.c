ba2029c2e50b7811ad65aacf0a9549db3189626b
ayush <ayush@localhost.localdomain>
2023-11-24 14:35:52 +0530

    Done

--- tasks/task.c
+++ tasks/task.c
@@ -1,66 +1,83 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-    int choice,total=0,quantity=0,exp;
-    char yn,item[100];
+// Function to calculate and print the total price for Pizza
+int calculatePizzaTotal(int quantity) {
+    int price = quantity * 90;
+    printf("Total Price: %d\n", price);
+    return price;
+}
 
+// Function to calculate and print the total price for Burger
+int calculateBurgerTotal(int quantity) {
+    int price = quantity * 89;
+    printf("Total Price: %d\n", price);
+    return price;
+}
+
+// Function to calculate and print the total price for Pani-Puri
+int calculatePaniPuriTotal(int quantity) {
+    int price = quantity * 100;
+    printf("Total Price: %d\n", price);
+    return price;
+}
+
+// Function to calculate and print the total price for Dosa
+int calculateDosaTotal(int quantity) {
+    int price = quantity * 80;
+    printf("Total Price: %d\n", price);
+    return price;
+}
+
+int main(void) {
+    int total = 0, quantity = 0, exp;
+    char yn, item[100];
 
     printf("============Welcome To TOPS Restaurent==============\n");
     printf("\tSr.No\tItem\t\tPrice\n");
     printf("\t1\tPizza\t\t90/-\n");
-    printf("\t2\tBurgur\t\t89/-\n");
+    printf("\t2\tBurger\t\t89/-\n");
     printf("\t3\tPani-Puri\t100/-\n");
-    printf("\t4\tDosha\t\t80/-\n");
+    printf("\t4\tDosa\t\t80/-\n");
 
-    while(yn!='n'){
+    // Loop to take orders
+    while (yn != 'n') {
         printf("Your Item: ");
-        scanf("%s",item);
-        
+        scanf("%s", item);
+
         printf("Quantity: ");
-        scanf("%d",&quantity);
+        scanf("%d", &quantity);
 
-        if(strcmp("Pizza",item) == 0){
-            exp = quantity*90;
-            total+=exp;
-            printf("Total Price: %d\n",quantity * 90);
-
-        }
-
-        else if(strcmp("Burgur",item) ==0){
-            exp = quantity * 89;
-            total+=exp;
-            printf("Total Price: %d\n",quantity * 89);
-
-
-        }
-        else if(strcmp("Pani-Puri",item) == 0){
-            exp = quantity * 100;
-            total +=exp;
-            printf("Total Price: %d\n",quantity * 100);
-
-        }
-        else{
+        // Check the selected item and call the appropriate function
+        if (strcmp("Pizza", item) == 0) {
+            exp = calculatePizzaTotal(quantity);
+        } else if (strcmp("Burger", item) == 0) {
+            exp = calculateBurgerTotal(quantity);
+        } else if (strcmp("Pani-Puri", item) == 0) {
+            exp = calculatePaniPuriTotal(quantity);
+        } else if (strcmp("Dosa", item) == 0) {
+            exp = calculateDosaTotal(quantity);
+        } else {
+            // For other items, use a generic function
             exp = quantity * 80;
-            total +=exp;
-            printf("Total Price: %d\n",quantity * 80);
-
+            printf("Total Price: %d\n", exp);
         }
-        while(getchar()!='\n');
 
+        // Accumulate the total price
+        total += exp;
 
-        printf("Do you want anything else ? [y/n]: ");
-        scanf("%c",&yn);
+        // Clear input buffer
+        while (getchar() != '\n');
 
-
-        
+        printf("Do you want anything else? [y/n]: ");
+        scanf(" %c", &yn);  // Add a space before %c to consume the newline character
     }
 
+    // Print the final receipt
     printf("\n--------------------------------\n");
     printf("Thank you\n");
-    printf("Your total is : %d\n",total);
+    printf("Your total is: %d\n", total);
     printf("\n--------------------------------\n");
 
-    
     return 0;
 }
--- tasks/task3.c
+++ tasks/task3.c
@@ -3,13 +3,28 @@
 
 #define PI 3.14159265359
 
+// Function to calculate and return the area of a circle
+float calculateCircleArea(float radius) {
+    return PI * radius * radius;
+}
+
+// Function to calculate and return the area of a triangle
+float calculateTriangleArea(float base, float height) {
+    return 0.5 * base * height;
+}
+
+// Function to calculate and return the area of a rectangle
+float calculateRectangleArea(float length, float width) {
+    return length * width;
+}
+
 int main() {
     int choice;
     float radius, length, width, area;
 
     printf("=============Area Finder=============\n");
     printf("\t1. Circle\n\t2. Triangle\n\t3. Rectangle\n");
-    
+
     printf("\nEnter Choice: ");
     scanf("%d", &choice);
 
@@ -19,7 +34,7 @@ int main() {
             printf("Enter radius: ");
             scanf("%f", &radius);
 
-            area = PI * radius * radius;
+            area = calculateCircleArea(radius);
 
             printf("Area of Circle: %f\n", area);
             break;
@@ -31,7 +46,7 @@ int main() {
             printf("Enter height: ");
             scanf("%f", &width);
 
-            area = 0.5 * length * width;
+            area = calculateTriangleArea(length, width);
 
             printf("Area of Triangle: %f\n", area);
             break;
@@ -43,7 +58,7 @@ int main() {
             printf("Enter width: ");
             scanf("%f", &width);
 
-            area = length * width;
+            area = calculateRectangleArea(length, width);
 
             printf("Area of Rectangle: %f\n", area);
             break;
